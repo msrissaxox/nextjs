83c28cedfe279cd094923e36f5d41bd9373fd792
msrissaxox <marissa.bianca.lamothe@gmail.com>
2024-12-28 18:19:17 -0500

    first commit

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Picture from './Picture';
+import Info from './Info';
+import { properties } from '../../details.js';
+
+
+export default function Card (){
+    return(
+<div>
+<p>Hello</p>
+
+</div>
+    )
+};
+
---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/";
+import Card from "./components/Card";
+import { CACHE_ONE_YEAR } from "next/dist/lib/constants";
+
+export default function Page() {
+  return (
+  <div>
+  <h1>Hello, Next.js!</h1>
+  <h1>Hello, Next.js!</h1>
+  <Card />
+  </div>
+  )
+}
+
